19692c887dfbd7599721038368abe1a6517fe76e
warlyware <danward@gmail.com>
2015-06-30 09:15:52 -0700

    First commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {  });
+});
+
+/* GET home page. */
+router.get('/mission', function(req, res, next) {
+  res.render('mission', {  });
+});
+
+
+module.exports = router;
